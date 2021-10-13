c0e417deec78a56c0e0ee3196f9525cb28043422
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-12 23:37:59 -0700

    resolvers.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,58 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError('Please sign in');
+        }
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user  = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('User not found!');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Wrong password!');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveDrink: async (parent, args, context) => {
+            const drink = { ...args };
+            if (context.user) {
+              const user = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedDrinks: drink } },
+                { new: true }
+              );
+              return user;
+            }
+            throw new AuthenticationError('Please sign in');
+        },
+        removeDrink: async (parent, { drinkId }, context) => {
+            if (context.user) {
+              const user = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedDrinks: { drinkId: drinkId } } },
+                { new: true }
+              );
+              return user;
+            }
+            throw new AuthenticationError('Please sign in');
+        },
+    }
+};
+
+module.exports = resolvers;
