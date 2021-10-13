ca87824e8cc9f295880241fc024464205d1cc682
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-12 21:40:41 -0700

    Drink subdocument schema

---
+++ server/models/Drink.js
@@ -0,0 +1,71 @@
+const { Schema } = require('mongoose');
+
+// Drink subdocument schema
+const DrinkSchema = new Schema({
+    drinkId: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+    tags: [
+        {
+            type: String,
+        },
+    ],
+    instructions: {
+        type: String,
+    },
+    ingredient1: {
+        type: String,
+    },
+    ingredient2: {
+        type: String,
+    },
+    ingredient3: {
+        type: String,
+    },
+    ingredient4: {
+        type: String,
+    },
+    ingredient5: {
+        type: String,
+    },
+    ingredient6: {
+        type: String,
+    },
+    ingredient7: {
+        type: String,
+    },
+    ingredient8: {
+        type: String,
+    },
+    ingredient9: {
+        type: String,
+    },
+    ingredient10: {
+        type: String,
+    },
+    ingredient11: {
+        type: String,
+    },
+    ingredient12: {
+        type: String,
+    },
+    ingredient13: {
+        type: String,
+    },
+    ingredient14: {
+        type: String,
+    },
+    ingredient15: {
+        type: String,
+    },
+});
+
+module.exports = DrinkSchema;
