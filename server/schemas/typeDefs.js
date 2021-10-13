ea57d0f6efa648004a72b73605f890abd315ce98
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-12 23:50:13 -0700

    fixed some typos, server runs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server-exporess');
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     input DrinkInput {
@@ -53,10 +53,10 @@ const typeDefs = gql`
     }
     type Auth {
         token: ID!
-        user: user
+        user: User
     }
     type Query {
-        me: user
+        me: User
     }
     type Mutation {
         login(email: String!, password: String!): Auth
