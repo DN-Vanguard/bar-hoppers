3c07e194ebca9764b0c63102049b9f3c05af68f8
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-18 20:07:26 -0700

    typo

--- client/src/components/Appbar.js
+++ client/src/components/Appbar.js
@@ -135,7 +135,7 @@ export default function Appbar({ currentPage, handlePageChange }) {
                                     <ListAltIcon />
                                 </ListItemIcon>
                                 <ListItemText primary={text} />
-                            </ListItemButton>sd
+                            </ListItemButton>
                         </List>
                     ))}
                     <List
