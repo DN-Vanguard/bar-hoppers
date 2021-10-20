4ca9299123d304634c8ca9bb904cd2cdbbb23947
ywkuo227 <ywkuo@outlook.com>
2021-10-19 23:15:18 -0700

    Corrected styling and rendering issue for Home Suggested Drinks, View All Suggested and created Explore by Letter.

--- client/src/components/pages/Letter.js
+++ client/src/components/pages/Letter.js
@@ -1,58 +1,39 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import React, { useEffect, useState } from 'react';
+// import Box from '@mui/material/Box';
+// import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
 
-export default function BasicButtons() {
+
+export default function Letter({ currentPage, handlePageChange }) {
+  const [stateLtrNum, setStateLtrNum] = useState([]);
+
+  useEffect(() => {
+    const objLtrNum = { 0: { orgLtrNum: "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" } }
+
+    const ltrNumArray = [...objLtrNum[0].orgLtrNum]
+
+    setStateLtrNum(ltrNumArray);
+
+  }, [setStateLtrNum])
+
+  // console.log(stateLtrNum);
+
+  const renderLtrNum = () => {
+    return (
+      stateLtrNum.map((character) => {
+        return (
+          <div key={character} className="buttonStyles" onClick={() => handlePageChange(character)}>
+            <Avatar alt={character} sx={{ width: 50, height: 50, zIndex: -1, fontSize: "x-large" }}>{character}</Avatar>
+          </div>
+        )
+      }))
+  };
+
   return (
     <div className="alphabetContainer">
-      <Box className="buttonStyles">
-        <Button variant="outlined">A</Button>
-        <Button variant="outlined">B</Button>
-        <Button variant="outlined">C</Button>
-        <Button variant="outlined">D</Button>
-        <Button variant="outlined">E</Button>
-        <Button variant="outlined">F</Button>
-      </Box>
-      <Box className="buttonStyles">
-        <Button variant="outlined"sx={{width: 40, height:40}}>G</Button>
-        <Button variant="outlined">H</Button>
-        <Button variant="outlined">I</Button>
-        <Button variant="outlined">J</Button>
-        <Button variant="outlined">K</Button>
-        <Button variant="outlined">L</Button>
-      </Box>
-      <Box className="buttonStyles">
-        <Button variant="outlined">M</Button>
-        <Button variant="outlined">N</Button>
-        <Button variant="outlined">PP</Button>
-        <Button variant="outlined">P</Button>
-        <Button variant="outlined">Q</Button>
-        <Button variant="outlined">R</Button>
-      </Box>
-      <Box className="buttonStyles">
-        <Button variant="outlined">S</Button>
-        <Button variant="outlined">T</Button>
-        <Button variant="outlined">U</Button>
-        <Button variant="outlined">V</Button>
-        <Button variant="outlined">W</Button>
-        <Button variant="outlined">X</Button>
-      </Box>
-      <Box className="buttonStyles">
-        <Button variant="outlined">Y</Button>
-        <Button variant="outlined">Z</Button>
-        <Button variant="outlined">0</Button>
-        <Button variant="outlined">1</Button>
-        <Button variant="outlined">2</Button>
-        <Button variant="outlined">3</Button>
-      </Box>
-      <Box className="buttonStyles">
-        <Button variant="outlined">4</Button>
-        <Button variant="outlined">5</Button>
-        <Button variant="outlined">6</Button>
-        <Button variant="outlined">7</Button>
-        <Button variant="outlined">8</Button>
-        <Button variant="outlined">9</Button>
-      </Box>
+      {renderLtrNum()}
     </div>
   );
 }
+
+
