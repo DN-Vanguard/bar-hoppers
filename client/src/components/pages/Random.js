cd84ca2400d1e96de5cba33c6133454dbab4897e
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-23 04:08:19 -0700

    drink detail fav save

--- client/src/components/pages/Random.js
+++ client/src/components/pages/Random.js
@@ -78,7 +78,7 @@ export default function Random({ currentPage, handlePageChange }) {
         />
         <IconButton onClick={() => handleSaveDrink(randomDrinkData)} style={{cursor:"pointer"}} >
             <BookmarkIcon />
-        </IconButton>                        
+        </IconButton>
       </div>
     );
   };
